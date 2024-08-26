76069cb6e7859b044c20fb436c1d9595073142b4
lorenzomene <lorenzospmene@gmail.com>
2024-08-26 16:09:25 -0300

    feat: parsing objects and values

--- parser.go
+++ parser.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 type Parser struct {
 	tokens  []Token
@@ -48,6 +51,78 @@ func (p *Parser) parseArray() {
 
 }
 
-func (p *Parser) parseObject() {
+func (p *Parser) parseObject() (map[string]interface{}, error) {
+	obj := make(map[string]interface{})
 
+	if p.nextToken().Type != LEFT_BRACE {
+		return nil, fmt.Errorf("unexpected token %v, expected '{'(LEFT_BRACE) at the start of an object", p.nextToken().Type)
+	}
+
+	for {
+		token := p.peekToken()
+
+		if token.Type == RIGHT_BRACE {
+			p.nextToken()
+			break
+		}
+
+		if token.Type != STRING {
+			return nil, fmt.Errorf("unexpected token type: %v, expected STRING", token.Type)
+		}
+		key := token.Value
+		p.nextToken()
+
+		if p.nextToken().Type != COLON {
+			return nil, fmt.Errorf("unexpected token type: %v, expected COLON after key: %v", token.Type, key)
+		}
+
+		value, err := p.parseValue()
+
+		if err != nil {
+			return nil, err
+		}
+		obj[key] = value
+
+		token = p.peekToken()
+		if token.Type == COMMA {
+			p.nextToken()
+		} else if token.Type != RIGHT_BRACE {
+			return nil, fmt.Errorf("expected COMMA or RIGHT_BRACE after value for key %v", key)
+		}
+	}
+
+	return obj, nil
+}
+
+func (p *Parser) parseValue() (interface{}, error) {
+	token := p.peekToken()
+
+	switch token.Type {
+	case STRING:
+		p.nextToken()
+		return token.Value, nil
+	case NUMBER:
+		p.nextToken()
+		num, err := strconv.ParseFloat(token.Value, 64)
+		if err != nil {
+			return nil, fmt.Errorf("invalid number format: %v", token.Value)
+		}
+		return num, nil
+	case TRUE:
+		p.nextToken()
+		return true, nil
+	case FALSE:
+		p.nextToken()
+		return false, nil
+	case NULL:
+		p.nextToken()
+		return nil, nil
+	case LEFT_BRACE:
+		return p.parseObject()
+	case LEFT_BRACKET:
+		// return p.parseArray()
+		return nil, nil
+	default:
+		return nil, fmt.Errorf("unexpected token: %v", token.Type)
+	}
 }
