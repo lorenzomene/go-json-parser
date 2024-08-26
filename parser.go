7448100106a6a4c42b7cf89081fb7eb2f703d9e3
lorenzomene <lorenzospmene@gmail.com>
2024-08-26 14:47:01 -0300

    feat: parser structure

--- parser.go
+++ parser.go
@@ -1,5 +1,53 @@
 package main
 
-func Parse() {
+import "fmt"
+
+type Parser struct {
+	tokens  []Token
+	current int
+}
+
+func NewParser(tokens []Token) *Parser {
+	return &Parser{
+		tokens:  tokens,
+		current: 0,
+	}
+}
+
+func (p *Parser) nextToken() Token {
+	if p.current < len(p.tokens) {
+		token := p.tokens[p.current]
+
+		p.current++
+
+		return token
+	}
+	return Token{Type: NULL, Value: ""}
+}
+
+func (p *Parser) peekToken() Token {
+	if p.current < len(p.tokens) {
+		return p.tokens[p.current]
+	}
+	return Token{Type: NULL, Value: ""}
+}
+
+func (p *Parser) Parse() (interface{}, error) {
+	token := p.peekToken()
+
+	switch token.Type {
+	case LEFT_BRACE:
+		p.parseObject()
+	case LEFT_BRACKET:
+		p.parseArray()
+	}
+	return nil, fmt.Errorf("unexpected token: %v, expected '{' or '['", token.Type)
+}
+
+func (p *Parser) parseArray() {
+
+}
+
+func (p *Parser) parseObject() {
 
 }
